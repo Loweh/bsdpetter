9122513bc0b22cce0b4355f2020511664a002a7f
Loweh <colder.than.zero@gmail.com>
2019-02-27 21:37:41 -0500

    it only took 4 months to work on this
    say hello to linked lists

---
+++ src/main.c
@@ -0,0 +1,33 @@
+#include "util/l_list.h"
+
+/*
+Compiling this thing is already a pain so just remember this:
+
+gcc main.c util/l_list.c -o test -std=c99 -w
+
+Adjust as necessary, but that's about the gist of it.
+*/
+
+int main() {
+	ll_node *head = NULL;
+	ll_create_head(&head, "meow");
+	ll_push_node(head, "meow2");
+	ll_push_node(head, "meow3");
+	
+    ll_node *node1, *node2;
+	node1 = ll_get_node(head, 1);
+	node2 = ll_get_node(head, 2);
+	
+	if (node1 == -1) {
+	    return 2;
+	} else if (node2 == -1) {
+	    return 3;  
+	};
+	
+	printf("%s\n%s\n%s\n", head->value, node1->value, node2->value);
+	
+	ll_remove_node(&head, 1);
+	
+	printf("%s\n%s\n", head->value, head->next->value);
+	return 0;
+};
---
+++ src/util/l_list.h
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/*!!!!!!!!!!!!!!!!!!!!!!!
+LL STANDS FOR LINKED LIST
+!!!!!!!!!!!!!!!!!!!!!!!*/
+
+typedef struct ll_node ll_node;
+
+struct ll_node {
+	char value[10];
+	struct ll_node *next;
+};
+
+void ll_create_head(ll_node **head, char value[10]);
+
+int ll_get_node(ll_node *head, int pos);
+
+void ll_push_node(ll_node *head, char value[10]);
+void ll_remove_node(ll_node **head, int pos);
